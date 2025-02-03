012bf01e8a65e2e20a355804371c7425fa1e0584
dinalans <dinalans@gmail.com>
2025-02-02 18:23:43 -0800

    update

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
+// admin.js
+
+// Import Firebase modules
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js';
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+} from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js';
+import {
+    getDatabase,
+    ref,
+    set,
+    update,
+    remove,
+    onValue,
+    push,
+} from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js';
+
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyB7yP6YD0oze6lo14u14aBKKVI8jBelHbc",
+const firebaseConfig = {
+    // Your Firebase configuration here
+    apiKey: "AIzaSyB7yP6YD0oze6lo14u14aBKKVI8jBelHbc",
     authDomain: "gaming-rental-site.firebaseapp.com",
     databaseURL: "https://gaming-rental-site-default-rtdb.firebaseio.com",
     projectId: "gaming-rental-site",
     storageBucket: "gaming-rental-site.firebasestorage.app",
     messagingSenderId: "573901873327",
     appId: "1:573901873327:web:949bea4a588b6f9a37745a"
+    // ... other configuration ...
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
-
-
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js';
-
-const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
 
 // Register Function
 window.register = function () {
@@ -241,20 +259,6 @@ function isGameAvailable(gameId, startDate, endDate, callback) {
         callback(isAvailable);
     });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Load available games on page load
 window.onload = function() {
